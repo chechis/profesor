6bf8220c73c801b518a05c6e1a75e5acb6079d44
chechis <jcalvarezg92@gmail.com>
2018-07-13 21:35:29 -0600

    menu funcionando

--- app/src/main/java/com/example/chechis/profesor/MainActivity.java
+++ app/src/main/java/com/example/chechis/profesor/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.example.chechis.profesor.fragmento.FragmentAsignatura;
+import com.example.chechis.profesor.fragmento.FragmentEstudiantes;
+import com.example.chechis.profesor.fragmento.FragmentTareas;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -16,9 +22,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_Layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,
+                new FragmentAsignatura()).commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_Layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -28,20 +56,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         switch (id){
 
             case R.id.nav1:
-
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,
+                        new FragmentAsignatura()).commit();
                 break;
 
             case R.id.nav2:
-
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,
+                        new FragmentEstudiantes()).commit();
                 break;
 
             case R.id.nav3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,
+                        new FragmentTareas()).commit();
 
                 break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_Layout);
         drawer.closeDrawer(GravityCompat.START);
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
