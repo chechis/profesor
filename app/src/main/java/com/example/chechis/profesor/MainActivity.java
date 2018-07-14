a63bcad42b5a4540586e83d2f8c1924359233a60
chechis <jcalvarezg92@gmail.com>
2018-07-13 20:41:40 -0600

    layout terminado
    solo falta el layout que es del fragmento para ingresar datos

--- app/src/main/java/com/example/chechis/profesor/MainActivity.java
+++ app/src/main/java/com/example/chechis/profesor/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.chechis.profesor;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x = 3;
+
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+
+            case R.id.nav1:
+
+                break;
+
+            case R.id.nav2:
+
+                break;
+
+            case R.id.nav3:
+
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_Layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return super.onOptionsItemSelected(item);
     }
 }
